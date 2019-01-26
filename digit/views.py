3984062a28b4abbfd269c9dc3280078702534b69
K-Yonhon <k.yonhon@gmail.com>
2019-01-26 18:50:30 +0900

    $.ajax({以降の部分は動いていない。 form action="" method="post"、input type="hidden" name="img" value="XXX" でデータがPOSTされているだけだった。

--- digit/views.py
+++ digit/views.py
@@ -1,34 +1,14 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 
-# from django.template.response import TemplateResponse
-# from django.http import HttpResponse, HttpResponseRedirect
-# from django.utils import timezone
-# from .models import Post
-# from io import BytesIO
-# from PIL import Image
-# import re
-# import base64
-
-
-@csrf_exempt
 def index(request):
     if request.method == 'POST':
 
-        data = request.POST['post_data_1']
+        data = request.POST['img']
         if data:
             val = 1
         else:
             val = 0
 
-        #image_width = int(request.POST['width'])
-        #image_height = int(request.POST['height'])
-        #image_data = re.sub("^data:image/png;base64,", "", image_data)
-        #image_data = base64.b64decode(image_data)
-        #image_data = BytesIO(image_data)
-        #im = Image.open(image_data)
-
-        # return HttpResponseRedirect('/', {'img': 'val'})
         return render(request, 'digit/index.html',  {'img': val})
     else:
         return render(request, 'digit/index.html', {})
