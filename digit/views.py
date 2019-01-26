fb7e71d86ff7b841a858925a32729e13377a3cb8
K-Yonhon <k.yonhon@gmail.com>
2019-01-26 22:30:14 +0900

    Uncaught ReferenceError: getCoookie is not defined http://ikapblg.blog.fc2.com/blog-entry-156.html

--- digit/views.py
+++ digit/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
+
+# @csrf_exempt
+# @csrf_protect
 def index(request):
     if request.method == 'POST':
+        from django.http import QueryDict
 
-        data = request.POST['img']
-        if data:
-            val = 1
-        else:
-            val = 0
+        # request.bodyに入っている。
+        dic = QueryDict(request.body, encoding='utf-8')
+        v1 = dic.get('key1')
+        v2 = dic.get('k2')
 
-        return render(request, 'digit/index.html',  {'img': val})
+        val = v1
+
+        # r1, r2 = do_something(v1, v2)
+
+        # from json import dumps
+        # ret = dumps({'k1': r1, 'k2': r2})
+        # return HttpResponse(ret, content_type='application/json')
+        return render(request, 'digit/index.html', {'img': val})
     else:
         return render(request, 'digit/index.html', {})
