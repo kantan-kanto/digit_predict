50f0ff1ca895f3133edaa77e84024890264a816a
K-Yonhon <k.yonhon@gmail.com>
2019-01-31 17:32:23 +0900

    dic_cnn,dic_dnnが送られない場合、500サーバーエラーとなっていたのに対応

--- digit/views.py
+++ digit/views.py
@@ -40,6 +40,8 @@ def index_j(request):
     if request.method == 'POST':
         data = request.POST.getlist('img')
         cvssize = data[0]
+        dic_cnn = {}
+        dic_dnn = {}
         if data[1]:
             val0 = data[1]
             val1_cnn = img_prp_cnn(val0)
@@ -48,18 +50,17 @@ def index_j(request):
 
             list_cnn = pb_cnn(val1_cnn)
             sort_cnn = list_cnn.argsort()
-            dic_cnn = {}
             for i in range(10):
                 dic_cnn[sort_cnn[0][i]] = '{:.5g}'.format(list_cnn[0][sort_cnn[0][i]])
 
             val1_dnn = img_prp(val0)
             list_dnn = pb_dnn(val1_dnn)
             sort_dnn = list_dnn.argsort()
-            dic_dnn = {}
             for i in range(10):
                 dic_dnn[sort_dnn[0][i]] = '{:.5g}'.format(list_dnn[0][sort_dnn[0][i]])
         else:
             val = None
+
         return render(request, 'digit/index_j.html',  {'img': val, 'cvssize': cvssize, 'list_cnn': dic_cnn, 'list_dnn': dic_dnn})
     else:
         return render(request, 'digit/index_j.html', {})
